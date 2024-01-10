2360c360df28b067588bb9f694a960ce547a0124
vitor-hugoo <vitorhugobarros@hotmail.com>
2024-01-10 11:58:32 -0300

    validacoes

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -5,7 +5,23 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+        
     def validate_cpf(self, cpf):
         if len(cpf) != 11:
             raise serializers.ValidationError("O CPF deve ter 11 dígitos")
         return cpf
+    
+    def validate_name(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Não inclua npumeros neste campo")
+        return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O Rg deve ter 9 dígitos")
+        return rg
+    
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError("O celular deve ter 11 dígitos")
+        return celular
