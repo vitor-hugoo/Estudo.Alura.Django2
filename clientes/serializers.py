e100d19128cbc0031b28ebf2cf29d8457b092f6f
vitor-hugoo <vitorhugobarros@hotmail.com>
2024-01-10 14:22:11 -0300

    validacoes fora do serializer

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,27 +1,21 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+    
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'CPF':"O CPF deve ter 11 dígitos"})
         
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
-        return cpf
-    
-    def validate_name(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("Não inclua npumeros neste campo")
-        return nome
-    
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("O Rg deve ter 9 dígitos")
-        return rg
-    
-    def validate_celular(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError("O celular deve ter 11 dígitos")
-        return celular
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': 'Não inclua números nesse campo'})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'RG': "O RG deve ter 9 dígitos"})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'Celular': "O celular deve ter 11 dígitos"})
