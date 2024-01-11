b449ad5dcbd7cd701c67606d8824fb55157121da
vitor-hugoo <vitorhugobarros@hotmail.com>
2024-01-11 08:36:40 -0300

    alteracao de paginacao offset para page

--- setup/settings.py
+++ setup/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-        'PAGE_SIZE': 10
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10,
 }
